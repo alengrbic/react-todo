304e7547919d483fdf3fbc7eb1dac9f366f11d7e
Alen Grbic <alengrbic90@gmail.com>
2019-07-15 14:13:39 +0100

    Updates

---
+++ precache-manifest.c9edefe2aa0ff4d6ff6fbac07bd61d2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5466d96cc542d5939a1f5d1b746a1ed",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "c6e92a1e145b55e81001",
+    "url": "/react-todo/static/css/main.8f27a0b7.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/react-todo/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "c6e92a1e145b55e81001",
+    "url": "/react-todo/static/js/main.3fa84d5c.chunk.js"
+  },
+  {
+    "revision": "052fad23ca9f036fcf47",
+    "url": "/react-todo/static/js/runtime~main.79666361.js"
+  }
+]);
